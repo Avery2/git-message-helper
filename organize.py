1ce8a085ea359af694f9b71130ff973ebc2d6334
Avery2 <justinaverychan@gmail.com>
2022-09-27 22:59:05 -0700

    initial commit 🚀

---
+++ organize.py
@@ -0,0 +1,47 @@
+
+import re
+my_filename = 'log.txt'
+
+# if __name__ == "__main__":
+with open(my_filename) as f:
+    lines = f.readlines()
+
+# go through lines, identify commit message vs file
+commit_regex = r"^\w{10} "
+# I want a list of tuples
+    # tuples will be (commit line, list of file paths)
+
+grouped_by_commit = []
+filenames = []
+
+for line in lines:
+    # remove newline at end of each line
+    line = line[:-1]
+    if re.findall(commit_regex, line):
+        # print(f"commit line found: {line}")
+        grouped_by_commit.append((line, []))
+    else:
+        if grouped_by_commit:
+            grouped_by_commit[-1][1].append(line)
+        filenames.append(line)
+
+filenames = set(filenames)
+# print(*grouped_by_commit, sep="\n")
+# print(filenames)
+
+grouped_by_filename = {}
+
+for commit, files in grouped_by_commit:
+    for filename in files:
+        if filename not in grouped_by_filename:
+            grouped_by_filename[filename] = []
+        grouped_by_filename[filename].append(commit)
+
+# print(*grouped_by_filename.items(), sep="\n")
+
+# printable version
+
+for filename, commits in grouped_by_filename.items():
+    print(f"- {filename}")
+    for commit in commits:
+        print(f"\t- {commit}")
