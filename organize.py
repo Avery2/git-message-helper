bb9af5eb360de45278f8fd14ff47821ad9247b1f
Avery2 <justinaverychan@gmail.com>
2022-10-02 16:11:30 -0700

    docs: update comments

--- organize.py
+++ organize.py
@@ -1,16 +1,15 @@
 
 import re
+
 my_filename = 'log.txt'
 
-# if __name__ == "__main__":
 with open(my_filename) as f:
     lines = f.readlines()
 
 # go through lines, identify commit message vs file
 commit_regex = r"^\w{7,10} "
-# I want a list of tuples
-    # tuples will be (commit line, list of file paths)
 
+# grouped_by_commit will be a list of tuples; tuples will be (commit line, list of file paths)
 grouped_by_commit = []
 filenames = []
 
@@ -18,7 +17,6 @@ for line in lines:
     # remove newline at end of each line
     line = line[:-1]
     if re.findall(commit_regex, line):
-        # print(f"commit line found: {line}")
         grouped_by_commit.append((line, []))
     else:
         if grouped_by_commit:
@@ -26,9 +24,6 @@ for line in lines:
         filenames.append(line)
 
 filenames = set(filenames)
-# print(*grouped_by_commit, sep="\n")
-# print(f"{filenames=}")
-# print(f"{grouped_by_commit=}")
 
 grouped_by_filename = {}
 
@@ -37,10 +32,6 @@ for commit, files in grouped_by_commit:
         if filename not in grouped_by_filename:
             grouped_by_filename[filename] = []
         grouped_by_filename[filename].append(commit)
-
-# print(*grouped_by_filename.items(), sep="\n")
-
-# printable version
 
 def styleAsCode(s):
     return f"<code>{s}</code>"
